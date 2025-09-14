2d3e20e16f0ec3b60999c277ff44150613f3c3a2
Artem4553 <moyaccaunt4527@gmail.com>
2025-09-14 14:59:09 +0300

    Kamennojnicibumaga.kt

--- Kamennojnicibumaga.kt
+++ Kamennojnicibumaga.kt
@@ -1,18 +1,16 @@
-import java.util.*
 import kotlin.random.Random
 
 fun main() {
-    val scanner = Scanner(System.`in`)
     val choices = arrayOf("Камень", "Ножницы", "Бумага")
 
     while (true) {
-        println("Сделайте свой выбор:")
+        println("\nСделайте свой выбор:")
         println("1 - Камень")
         println("2 - Ножницы")
         println("3 - Бумага")
         print("Введите номер: ")
 
-        val playerChoice = readInt(scanner)
+        val playerChoice = readInt()
         if (playerChoice == null || playerChoice < 1 || playerChoice > 3) {
             println("Ошибка: Введите число от 1 до 3.")
             continue
@@ -20,38 +18,37 @@ fun main() {
 
         val computerChoice = Random.nextInt(0, 3) // 0, 1 или 2
 
-        println("Ваш выбор: ${choices[playerChoice - 1]}")
+        println("\nВаш выбор: ${choices[playerChoice - 1]}")
         println("Выбор компьютера: ${choices[computerChoice]}")
 
         val result = determineWinner(playerChoice - 1, computerChoice)
         println(result)
 
         if (result.contains("Ничья")) {
-            println("Переигрываем...")
+            println("Переигрываем...\n")
             continue
         }
 
-        break 
+        break
     }
 }
 
 fun determineWinner(player: Int, computer: Int): String {
     return when {
         player == computer -> "Ничья!"
-        (player == 0 && computer == 1) || 
-                (player == 1 && computer == 2) || 
-                (player == 2 && computer == 0) ->  
-            "Вы победили!"
+        (player == 0 && computer == 1) ||
+                (player == 1 && computer == 2) ||
+                (player == 2 && computer == 0) -> "Вы победили!"
         else -> "Вы проиграли!"
     }
 }
 
-fun readInt(scanner: Scanner): Int? {
+fun readInt(): Int? {
     try {
-        return scanner.nextLine().toInt()
+        return readLine()?.toIntOrNull()
     } catch (e: NumberFormatException) {
         println("Ошибка: Введите целое число.")
         return null
     }
-
 }
+
